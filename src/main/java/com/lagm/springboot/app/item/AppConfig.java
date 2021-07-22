b61019ec6705e8f511dcd141e60033784cc75953
Luis Guisado <lguisadom@gmail.com>
2021-07-21 23:52:12 -0500

    Uso Ribbon para balanceo de carga en Rest Template

--- src/main/java/com/lagm/springboot/app/item/AppConfig.java
+++ src/main/java/com/lagm/springboot/app/item/AppConfig.java
@@ -1,5 +1,6 @@
 package com.lagm.springboot.app.item;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -8,6 +9,7 @@ import org.springframework.web.client.RestTemplate;
 public class AppConfig {
 	// Registra el RestTemplate para usarlo mediante Autowired
 	@Bean(name = "clienteRest")
+	@LoadBalanced // Permite utilizar Ribbon para balanceo de carga, usando RestTemplate
 	public RestTemplate registrarRestTemplate() {
 		return new RestTemplate();
 	}
