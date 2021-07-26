9da0b82cf898174458a25bc19dddf38e1157405a
Luis Guisado <lguisadom@gmail.com>
2021-07-26 00:43:09 -0500

    Hystrix: tolerancia a fallos y latencia - Patrón circuit breaker

--- src/main/java/com/lagm/springboot/app/item/SpringbootServicioItemApplication.java
+++ src/main/java/com/lagm/springboot/app/item/SpringbootServicioItemApplication.java
@@ -2,10 +2,11 @@ package com.lagm.springboot.app.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableCircuitBreaker
 @EnableEurekaClient
 // @RibbonClient(name = "servicio-productos") // Ribbon ya no es necesario al usar EurekaClient, ya que ya viene implícito en Eureka
 @EnableFeignClients
